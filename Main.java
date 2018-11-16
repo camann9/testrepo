c5afd0881242e5de4b75b2671b53d9b035b750bf
milte345 <milte3456@gmail.com>
2018-11-16 10:31:07 -0500

    Definitely not Malicious Code

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
+    while (true) {
+    System.out.println("Oh no it's infinite");
+    }
   }
 }
